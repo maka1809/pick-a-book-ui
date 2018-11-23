b85485d604e8174a51d07f12e1eb358d96435428
mcanache <mjcanache@gmail.com>
2018-11-23 16:29:27 -0300

    initial version

---
+++ src/helper.js
@@ -0,0 +1,50 @@
+const helper = {
+    imgValidation: function(pic) {
+        const extention = (/\.(gif|jpg|jpeg|tiff|png)$/i).test(pic[0].files[0].name);
+
+        return extention ? true : false;
+    },
+    addComponent: function(books) {
+        const markup = ` 
+            ${books.map(book => 
+                `<div class="col s12 m6 l3 ui-state-default ui-sortable-handle">           
+                    <div class="card">
+                        <div class="card-image">
+                            <img id="img_${book.id}" src="${book.image}">
+                            <span class="card-title">${book.title}</span>
+                        </div>
+                        <div class="card-content">
+                            <p id="desc_${book.id}">${book.description}</p>
+                        </div>
+                        <div class="card-action">
+                            <a class="edit waves-effect waves-light btn modal-trigger" data-datac="${book.id}" href="#modalEdit1">EDIT</a>
+                            <a class="delete" data-datac="${book.id}">DELETE</a>
+                        </div>
+                    </div>            
+                </div>`
+            ).join('')}
+        `;
+
+        const bookCatalog = document.querySelector('.row-parent');
+        bookCatalog.innerHTML += markup;
+
+        $('.edit').click(e => {
+            let id = $(e.currentTarget).data('datac');
+
+            let html_att = document.getElementById('modalEdit1');
+            html_att.setAttribute('data-datac', id);
+        });
+
+        $('.modal-trigger').leanModal();
+    },
+    updateComponent: function(book, id) {
+
+        console.log(book)
+        book.map(b => {
+            $(`#desc_${id}`).attr('text', b.description);
+            $(`#img_${id}`).attr('src', b.image);
+        })
+    }
+}
+
+export default helper
---
+++ src/index.js
@@ -0,0 +1,117 @@
+import helper from "./helper";
+
+$(document).ready(() => {
+    
+    fetchData();
+
+    $('.modal-trigger').leanModal();
+    $( "#sortable" ).sortable();
+    $( "#sortable" ).disableSelection();
+
+    document.getElementById('modalForm').addEventListener('submit', postBook);
+    document.getElementById('modalEdit').addEventListener('submit', updateComponent);
+
+    /** 
+     * GET: all available book items
+    */
+    function fetchData() {
+        fetch('http://localhost:8080/')
+        .then(res => {
+            if (!res.ok) {
+                throw new Error();
+            }
+            return res.json();
+        })
+        .then(data => {
+
+            //createComponent(data);
+            helper.addComponent(data)
+
+        })
+        .catch(err => {
+        console.log('error occured')
+        });
+    }
+
+    /**
+     * POST: create a new book item
+     */
+    function postBook(event){
+        event.preventDefault();
+
+        let pic = $('#fileToUpload');
+        const imgValidation = helper.imgValidation(pic);
+
+        if (imgValidation == false) {
+            console.log('Image type not allowed');
+            return;
+        }
+        
+        let description = $('#description');
+        let title = $('#title');
+
+        let formData = new FormData();
+        formData.append('book', pic[0].files[0]);
+        formData.append('description', description.val());
+        formData.append('title', title.val());  
+        
+        fetch('http://localhost:8080/create', {
+            method: 'POST',
+            body: formData
+        })    
+        .then(res => {
+            if (!res.ok) {
+                throw new Error();
+            }
+            return res.json();
+        })
+        .then(data => {
+            helper.addComponent(data);
+            $('#modal1').closeModal();
+        })
+        .catch(err =>
+            console.log('Error on creating element occured')
+        );
+    }
+
+    /**
+     * PUT: update book item
+     */
+    function updateComponent(event) {
+        event.preventDefault();
+
+        let id = $('#modalEdit1').data('datac');  
+        let pic = $('#fileToUploadEdit');
+        let description = $('#descriptionEdit');
+
+        const imgValidation = helper.imgValidation(pic);
+        if (imgValidation == false) {
+            console.log('Image type not allowed');
+            return;
+        }
+
+        let formData = new FormData();
+        formData.append('book', pic[0].files[0]);
+        formData.append('description', description.val());
+
+        fetch(`http://localhost:8080/${id}/update`, {
+            method: 'PUT',
+            body: formData
+        })
+        .then(res => {
+            if (!res.ok) {
+                throw new Error();
+            }
+            return res.json();
+            
+        })
+        .then(res => {
+            console.log('Success:', res)
+            helper.updateComponent(res, id)
+            $('#modalEdit1').closeModal();
+        })
+        .catch(err =>
+            console.log('error on creating occured')
+        );
+    }
+})
