1e30a7409e5477a69b03e16e62dc3298a0747c41
Maria Canache <mjcanache@gmail.com>
2023-04-17 17:03:49 -0400

    [fix] remove extra space

--- src/helper.js
+++ src/helper.js
@@ -1,16 +1,17 @@
-import { HOST_URL } from "../config"
+import { HOST_URL } from '../config';
 
 const helper = {
-
   books: [],
-  imgValidation: function(pic) {
-    const extention = (/\.(gif|jpg|jpeg|tiff|png)$/i).test(pic[0].files[0].name);
+  imgValidation: function (pic) {
+    const extention = /\.(gif|jpg|jpeg|tiff|png)$/i.test(pic[0].files[0].name);
     return extention ? true : false;
   },
-  addComponent: function(books) {
+  addComponent: function (books) {
     const markup = ` 
-        ${books.map(book => 
-            `<div class="col s12 m6 l3 ui-state-default ui-sortable-handle" id="component_${book.id}">           
+        ${books
+          .map(
+            (book) =>
+              `<div class="col s12 m6 l3 ui-state-default ui-sortable-handle" id="component_${book.id}">           
                 <div class="card">
                     <div class="card-image">
                         <img id="img_${book.id}" src="${book.image}">
@@ -24,69 +25,67 @@ const helper = {
                         <a class="delete waves-effect waves-light btn" id="delete" data-datac="${book.id}">DELETE</a>
                     </div>
                 </div>            
-            </div>`
-        ).join('')}
+            </div>`,
+          )
+          .join('')}
     `;
 
-    const bookCatalog = document.querySelector('.row-parent')
-    bookCatalog.innerHTML += markup
+    const bookCatalog = document.querySelector('.row-parent');
+    bookCatalog.innerHTML += markup;
 
-    $('.edit').click(e => {
-      let id = $(e.currentTarget).data('datac')
+    $('.edit').click((e) => {
+      let id = $(e.currentTarget).data('datac');
 
-      let html_att = document.getElementById('modalEdit1')
-      html_att.setAttribute('data-datac', id)
+      let html_att = document.getElementById('modalEdit1');
+      html_att.setAttribute('data-datac', id);
     });
 
-    $('.delete').click(e => {
-      let id = $(e.currentTarget).data('datac')
-      this.deleteComponent(id)
+    $('.delete').click((e) => {
+      let id = $(e.currentTarget).data('datac');
+      this.deleteComponent(id);
     });
 
-    $('.modal-trigger').leanModal()
+    $('.modal-trigger').leanModal();
   },
-  updateComponent: function(book, id) {
-    book.map(b => {
-      $(`#desc_${id}`).text(b.description)
-      $(`#img_${id}`).attr('src', b.image)
+  updateComponent: function (book, id) {
+    book.map((b) => {
+      $(`#desc_${id}`).text(b.description);
+      $(`#img_${id}`).attr('src', b.image);
+    });
+  },
+  deleteComponent: function (id) {
+    fetch(HOST_URL + `books/${id}`, {
+      method: 'DELETE',
     })
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error();
+        }
+
+        $(`#component_${id}`).remove();
+        this.decreaseCounter(id);
+        Materialize.toast('Card removed!', 3000);
+      })
+      .catch((err) => this.notification('We could not processed your request. Please try again.'));
   },
-  deleteComponent: function(id) {
-    fetch(HOST_URL+`books/${id}`, {
-      method: 'DELETE'
-    })    
-    .then(res => {
-      if (!res.ok) {
-        throw new Error()
-      }
-      
-      $(`#component_${id}`).remove()
-      this.decreaseCounter(id)
-      Materialize.toast('Card removed!', 3000)
-      
-    })
-    .catch(err =>
-      this.notification('We could not processed your request. Please try again.')
-    );
+  counter: function (data) {
+    data.map((d) => {
+      this.books.push(d);
+    });
+    this.setCounter();
   },
-  counter: function(data) {
-    data.map(d => {
-      this.books.push(d)
-    })
-    this.setCounter()    
+  decreaseCounter: function (id) {
+    let index = this.books.findIndex((p) => p.id == id);
+    this.books.splice(index, 1);
+    this.setCounter();
   },
-  decreaseCounter: function(id){
-    let index = this.books.findIndex(p => p.id == id)
-    this.books.splice(index, 1)
-    this.setCounter()
+  setCounter: function () {
+    $('#counter').text('Available books: ' + this.books.length);
   },
-  setCounter: function() {
-    $('#counter').text('Available books: ' + this.books.length)
-  },
-  notification: function(msg) {
-    $('#msg').text(msg)
+  notification: function (msg) {
+    $('#msg').text(msg);
     $('#notification').openModal();
-  }
-}
+  },
+};
 
-export default helper
+export default helper;
